dacc9c43c58f76e375cba2956a5164c50cd1dfee
qinglin89 <316032931@qq.com>
2024-08-07 12:04:29 +0800

    v0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gb
+
+go 1.21.9
+
+require github.com/bitdaddy89/ga v0.0.0-20240804160434-6529bd3cd95c
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bitdaddy89/ga/pkg"
+)
+
+func main() {
+	pkg.Say()
+	fmt.Println("vim-go")
+}
